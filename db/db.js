05f75099914e29a2e2c2291a2dba9e1dc1216a35
Nnacyv54 <nancyvsqz8@gmail.com>
2024-04-16 19:38:58 -0500

    Numero dos commit

---
+++ db/db.js
@@ -0,0 +1,26 @@
+// Importamos el modulo Mongoose para conectarnos a la base de datos de MONGODB.
+const mongoose = require("mongoose");
+// Establecemos nuestra url de la base de datos == DB
+const mongoDBURL =
+  "SU URL";
+
+// Funcion para conectarnos a la DB
+function connectDB() {
+  return new Promise((res, rej) => {
+    // Conectar a la base de datos usando la URL proporcioanada
+    mongoose
+      .connect(mongoDBURL)
+      .then(() => {
+        console.log("Conexion a la DB establecida correctamente");
+        // Si la conexion es exitosa resolvemos la promesa-
+        res();
+      })
+      .catch((err) => {
+        // Si hay un error al conectar, imprimir el error y rechazar la promesa
+        console.error("Error al conectar a la DB ", err);
+        rej(err);
+      });
+  });
+}
+//Exportamos la funcion de la conexion a la base de datos para poder utilizarla en el app.js
+module.exports = connectDB;
